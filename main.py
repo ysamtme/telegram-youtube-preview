c22691752ba0846ec1fc3c1a291c979702c0424d
MelomanCool <melomancool@gmail.com>
2017-10-08 16:09:29 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import os
+from io import BytesIO
+from time import time
+import logging
+
+import youtube_dl
+from ffmpy import FFmpeg
+from pygogo import Gogo
+from telegram.ext import Updater, RegexHandler
+import telegram
+
+from config import TOKEN
+
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = Gogo(
+    __name__,
+    low_formatter=formatter,
+    high_formatter=formatter
+).logger
+
+
+def get_videofile_url(youtube_url):
+    options = dict(quiet=True)
+    with youtube_dl.YoutubeDL(options) as ydl:
+
+        r = ydl.extract_info(youtube_url, download=False)
+        best_format = r['formats'][-1]
+
+        return best_format['url']
+
+
+def download_clip(url, start, length='10'):
+    try:
+        ext = 'mp4'
+        out_file_path = '{name}.{ext}'.format(name=time(), ext=ext)
+
+        ff = FFmpeg(
+            inputs={url: ['-ss', start]},
+            outputs={out_file_path: ['-t', length, '-c', 'copy', '-avoid_negative_ts', '1']},
+            global_options='-v quiet'
+        )
+        logger.info(ff.cmd)
+        ff.run()
+
+        with open(out_file_path, 'rb') as f:
+            out_file = BytesIO(f.read())
+            out_file.seek(0)
+        os.remove(out_file_path)
+
+        return out_file
+    except Exception as e:
+        logger.exception(e)
+
+
+def handle_link(bot, update, groupdict):
+    message = update.message
+
+    start = '{min}:{sec}'.format(**groupdict)
+    youtube_url = groupdict['url']
+    length = groupdict['length']
+    logger.info('Url: %s, start: %s, lenght: %s', youtube_url, start, length)
+    if not length:
+        length = '10'
+
+    bot.send_chat_action(message.chat.id, telegram.ChatAction.UPLOAD_VIDEO)
+
+    url = get_videofile_url(youtube_url)
+    downloaded_file = download_clip(url, start, length)
+
+    message.reply_video(downloaded_file, quote=False)
+
+
+def error_handler(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+if __name__ == '__main__':
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+
+    pattern = (
+        r'.*'
+         '(?P<url>(?:https?://)youtu\.be/[A-Za-z0-9_-]{11})'
+         '\?t='
+         '(?P<min>\d{1,2})'
+         'm'
+         '(?P<sec>\d{1,2})'
+         's'
+         '(?:\s+(?P<length>\d+))?'
+    )
+
+    dp.add_handler(RegexHandler(pattern, handle_link, pass_groupdict=True))
+
+    dp.add_error_handler(error_handler)
+
+    updater.start_polling()
+    updater.idle()
