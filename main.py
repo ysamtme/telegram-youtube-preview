1156d9c0cf3c6d84efa52a6cb3f93600fb629fde
MelomanCool <melomancool@gmail.com>
2018-09-16 15:09:08 +0400

    Don't delete the query message after sending the video

--- main.py
+++ main.py
@@ -115,11 +115,7 @@ def handle_link(bot, update, groupdict):
         file_url = get_videofile_url('https://youtu.be/' + request_info.video_id)
         downloaded_file = download_clip(file_url, request_info.start, request_info.end)
 
-        bot.send_video(message.chat_id, downloaded_file, caption=message.text)
-        try:
-            bot.delete_message(message.chat_id, message.message_id)
-        except telegram.error.BadRequest:
-            pass  # can't delete, ignore
+        bot.send_video(message.chat_id, downloaded_file, reply_to_message_id=message.message_id)
     except Exception as e:
         logger.exception(e)
 
